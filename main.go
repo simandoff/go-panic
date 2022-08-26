4ddc482f9e58c97427630e797263249671278d17
Boris Simandoff <simandoff@gmail.com>
2022-08-27 01:30:00 +0300

    Small changes

--- main.go
+++ main.go
@@ -51,15 +51,15 @@ func domemalloc() {
 			println("panic for: ", err)
 		}
 	}()
-	slimslice := make([]int64, 1<<20)
-	fatslice := make([]int64, 0, (1<<20)*10) // 50MB// /*1024*1024*1024/2*/
+	slimslice := make([]int64, 1<<20)        // 1MB
+	fatslice := make([]int64, 0, (1<<20)*10) // 10MB	//*1024*1024*1024/2*/
 	start := time.Now()
 	for {
 		fatslice = append(fatslice, slimslice...)
-		if len(fatslice)+len(slimslice) > cap(fatslice) {
-			size := len(fatslice) * 8
-			t := time.Since(start).Seconds()
-			fmt.Printf("%d MB at %.2f = %.2f MB/s\n", size>>20, t, float64(size)/(1<<20)/t)
-		}
+		// if len(fatslice)+len(slimslice) > cap(fatslice) {
+		size := len(fatslice) * 8
+		t := time.Since(start).Seconds()
+		fmt.Printf("%d MB at %.2f = %.2f MB/s\n", size>>20, t, float64(size)/(1<<20)/t)
+		// }
 	}
 }
